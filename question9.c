57d8e1cc061a5c546330e5520fb3f4ab6f5a669b
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 18:16:19 +0530

    c

---
+++ question9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int N,fp,sp,tp;
+    scanf("%d",&N);
+    fp=pow(N,1);
+    sp=pow(N,2);
+    tp=pow(N,3);
+    printf("%d %d %d",fp,sp,tp);
+	return 0;
+}
